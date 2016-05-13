c8ad0265035b204432bd0b837edc30d0ee2a8a10
Miguel Gonzalez <fuermiguel@yahoo.es>
2016-05-13 14:51:13 +0100

    solucionado error borrando estas etiquetas <maps/>

--- src/sakila/util/HibernateUtil.java
+++ src/sakila/util/HibernateUtil.java
@@ -1,37 +1,35 @@
-package sakila.util;
-
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+package sakila.util;
 
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
 
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+
 /**
- * Hibernate Utility class with a convenient method to get Session Factory
- * object.
- *
- * @author Miguel
+ * @author Deepak Kumar 
+ * * Web: http://www.roseindia.net
  */
 public class HibernateUtil {
-
     private static final SessionFactory sessionFactory;
-    
+
     static {
-        try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
+            try {
+            	Configuration cfg = new Configuration().configure("hibernate.cfg.xml");        	
+            	StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
+            	sb.applySettings(cfg.getProperties());
+            	StandardServiceRegistry standardServiceRegistry = sb.build();           	
+            	sessionFactory = cfg.buildSessionFactory(standardServiceRegistry);      	
+            } catch (Throwable th) {
+                    System.err.println("Enitial SessionFactory creation failed" + th);
+                    throw new ExceptionInInitializerError(th);
+            }
     }
-    
     public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+            return sessionFactory;
     }
 }
